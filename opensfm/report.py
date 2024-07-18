f68a7d1f874402a9d6e08136cbdb4b408acf14d6
Gabrielle Louise Gallo Martines <gaby@louiselog.net>
2024-07-18 18:03:49 -0300

    Atualizacao de texto

--- opensfm/report.py
+++ opensfm/report.py
@@ -151,7 +151,7 @@ class Report:
         # title
         self.pdf.set_font("Helvetica", "B", self.title_size)
         self.pdf.set_text_color(*self.mapi_light_green)
-        self.pdf.cell(0, self.margin, "ODM Quality Report", align="C")
+        self.pdf.cell(0, self.margin, "Relatorio de Qualidade da Geonex", align="C")
         self.pdf.set_xy(self.margin, self.title_size)
 
         # version number
@@ -276,7 +276,7 @@ class Report:
             geo_errors.append(f"{(self.stats['align']['coarse']['rmse_3d'] + self.stats['align']['fine']['rmse_3d']):.2f}")
         
         if len(geo_errors) > 0:
-            row_gps_gcp.append(" / ".join(geo_errors) + " meters")
+            row_gps_gcp.append(" / ".join(geo_errors) + " Metros")
             rows.append(row_gps_gcp)
 
         self._make_table(None, rows, True)
